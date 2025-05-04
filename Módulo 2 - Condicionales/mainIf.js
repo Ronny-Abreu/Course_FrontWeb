0b8c6948fb4f4df0c920249f892675906d24241f
tuerre <ronnyduran35@gmail.com>
2025-05-03 23:28:29 -0400

    Ennumerate of exercises conditional " if "

---
+++ Módulo 2 - Condicionales/mainIf.js
@@ -0,0 +1,12 @@
+// Ejercicios de condicionales if en JavaScript:
+
+// 1. Número positivo o negativo
+// 2. Mayor de edad
+// 3. Número par o impar
+// 4. Calificación aprobatoria
+// 5. Día de la semana
+// 6. Mayor de dos números
+// 7. Hora del día
+// 8. Número en un rango
+// 9. Meses del año
+// 10. Multiplicación de dos números solo si es mayor a 100
